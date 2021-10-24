475dcac89ff1adf1fc2b421ae522bf5eae9b365f
Shai Amir <shaiamir22@gmail.com>
2021-10-24 12:30:42 +0300

    day4 completed

--- week3/day4/script.js
+++ week3/day4/script.js
@@ -1,12 +1,12 @@
 let users = ["Lea123", "Princess45", "cat&doglovers", "helooo@000"]
 
 console.log(users.length)
-if(!users.length){
+if (!users.length) {
     console.log("no one online")
-}else if(users.length===1){
-    console.log(users[0]+"is online")
-}else if(users.length===2){
-    console.log(users[0]+"and"+users[1]+"is online")
-}else{
-    console.log(users[0]+" and "+users[1]+" and more "+(users.length-2)+"are on line")
+} else if (users.length === 1) {
+    console.log(users[0] + "is online")
+} else if (users.length === 2) {
+    console.log(users[0] + "and" + users[1] + "is online")
+} else {
+    console.log(users[0] + " and " + users[1] + " and more " + (users.length - 2) + "are on line")
 }
