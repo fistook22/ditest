e9a78c72a91ff468996718b3c033d4cfe68f7850
Shai Amir <shaiamir22@gmail.com>
2021-11-08 20:02:07 +0200

    python 1

---
+++ week5/day5/dc/todo/script.js
@@ -0,0 +1,10 @@
+let listTasks = [];
+let newTask = document.getElementsByTagName("button").innerHTML;
+
+
+function addTask() {
+    if (newTask = true) {
+        listTasks.appendchild("newTask");
+        setAttribute("checkbox").newTask;
+    }
+}
---
+++ week6/day2/hello.py.py
@@ -0,0 +1,48 @@
+#ex1
+print('Hello World ' * 4)
+
+#ex2
+(99 ** 3) * 8
+
+#ex3
+False
+True
+False
+True
+False
+5 < 3
+3 == 3
+3 == '3'
+# '3' > 3
+'Hello' == 'hello'
+
+#ex4
+computer_brand = 'Asus'
+print(f'I have an {computer_brand} computer')
+
+# #ex5
+name = 'Shai'
+age = 30
+shoe_size = 43
+info = f"my name is {name}, i am {age} years old, i wear {shoe_size} size shoes."
+print(info)
+
+#ex6
+a = 4
+b = 3
+if (a > b): print('Hello World')
+
+#ex7
+number = int(input('please enter a number')) 
+number % 2
+
+#ex8
+user_name = input("what's your name?")
+
+#ex9
+user_height = int(input("how tall are you?"))
+height_limit = 145
+if user_height >= height_limit:
+    print("You are tall enough to enjoy this ride!")
+elif user_height < height_limit:
+    print("You are not tall enough to go on that ride, try again when your higher.")
